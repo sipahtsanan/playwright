9d3cd0d2ebf905ea7035a03fcdb98dd6dc144c7d
SIPAHTSANAN KITTITHANATKUN <81869508+sipahtsanan@users.noreply.github.com>
2023-10-21 23:34:45 +0700

    Update tc_signin_not_succuess.spec.js

--- tests/tc_signin_not_succuess.spec.js
+++ tests/tc_signin_not_succuess.spec.js
@@ -7,10 +7,10 @@ test('verify sign in not success', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle("X. It’s what’s happening / X");
 
-  //   // Click the sign in menu.
+  //   // Click the sign-in menu.
   await page.getByRole('link', { name: 'Sign in' }).click();
 
-  //   // Expects page to have a heading with the name of Installation.
+  //   // Expects page to have a heading with the name 'Sign in to X'.
   await expect(page.getByRole('heading', { name: 'Sign in to X' })).toBeVisible();
 
   //  // Click the close or x button at the left-hand corner
